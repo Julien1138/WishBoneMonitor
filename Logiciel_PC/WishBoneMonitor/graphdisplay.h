47872a97a861f5c6f6f7b31f9b704de6e2c381dc
Julien1138 <Julien1138@users.noreply.github.com>
2010-09-19 22:20:58 +0000

    Logiciel PC : Affichage des courbes sur reception des trames séries.

--- Logiciel_PC/WishBoneMonitor/graphdisplay.h
+++ Logiciel_PC/WishBoneMonitor/graphdisplay.h
@@ -4,36 +4,47 @@
 #include <QWidget>
 #include <QGroupBox>
 #include <QGridLayout>
+#include <QList>
 #include <qwt_plot.h>
 #include <qwt_plot_curve.h>
+#include "wishboneregister.h"
+#include "PanelDoc.h"
 
 class GraphDisplay : public QWidget
 {
     Q_OBJECT
 public:
-    explicit GraphDisplay(QWidget *parent = 0);
+    explicit GraphDisplay(PanelDoc *pDoc, QWidget *parent = 0);
+    ~GraphDisplay();
 
     void UpdateDisplay(QRect);
 
+    QList<WishBoneRegister*>*   GetpRegisterList(){return &m_RegisterList;}
+    QList<QwtPlotCurve*>*       GetpCurveList(){return &m_CurveList;}
+    QwtPlot*                    GetPlot(){return m_pPlot;}
+
 signals:
     void Delete(GraphDisplay* pReg);
+//    void AddNew();
 
 public slots:
-
-private slots:
     void Delete();
+    void AddCurve();
+//    void AddOther();
 
 private:
     void contextMenuEvent(QContextMenuEvent * event);
     QAction*        m_pDeleteAction;
+    QAction*        m_pAddCurveAction;
+    QAction*        m_pAddGraphAction;
 
     QGroupBox       m_GroupBox;
     QGridLayout     m_Layout;
 
-    QwtPlot*        m_pPlot;
-    QwtPlotCurve*   m_pCurve;
-    double*         m_pX;
-    double*         m_pY;
+    QwtPlot*                    m_pPlot;
+    PanelDoc*                   m_pDoc;
+    QList<WishBoneRegister*>    m_RegisterList;
+    QList<QwtPlotCurve*>        m_CurveList;
 
 };
 
